4ec7eedac0e90a8fb4b7301263454601962edda9
Andrew Ormond <drewthedev9@gmail.com>
2019-04-05 17:42:44 -0400

    fs mudules done

--- fs.js
+++ fs.js
@@ -3,9 +3,42 @@ const fs = require('fs');
 
 
 // fs moule ha readFile method -> will read the hello.txt file.
-fs.readFile('./hello,txt', (err, data)=>{
+// if you reading a file for texts and anything for express. 
+// doesnt block the excecution
+fs.readFile('./hello.txt', (err, data)=>{
     if (err){
-        console.log('errrorrrr')
+        console.log('errrorrrr');
     }
-    console.log(data);
+    // toString takes the utf8 parmeter by default. does all numbers and languages.
+    //  ig you want to use utf8 use quotes 'utf8'.
+    console.log('async', data.toString());
+})
+
+//  File synce makes other stuff wait then readfile goes.
+const file = fs.readFileSync('./hello.txt');
+console.log('sync',file.toString());
+
+
+//appends text to the hello.txt file.
+// fs.appendFile('./hello.txt', ' hows things going?', err => {
+//     if (err){
+//         console.log('error')
+//     }
+// })
+
+// WRITE and adds a file with text in it
+
+fs.writeFile('bye.txt', 'sad to see you go!', err =>{
+    if (err){
+        consol1.log(err)
+    }
+})
+
+
+// delete the files you want.
+fs.unlink('./bye.txt', err =>{
+    if (err){
+        console.log(err)
+    }
+    console.log('bye.txt gone')
 })
