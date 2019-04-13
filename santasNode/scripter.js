8da7c99eb1c1cbb0dd01347d0eda6876f265937d
Andrew Ormond <drewthedev9@gmail.com>
2019-04-13 00:38:47 -0400

    1st wuestion done

--- santasNode/scripter.js
+++ santasNode/scripter.js
@@ -6,15 +6,34 @@ const fs = require('fs');
 
 function question1(){
 fs.readFile('./santa.txt', (err, data)=>{
+    // see time to excecute code 
+    console.time('santa-time');
     const directions = data.toString();
+    
     //  split the "()" up with the [;it split function"
-    const directionsArray = directions.split('');
-    const answer = diretionsArray.reduce((accumulter, currentValue)=> {
-        
-    })
-    console.log(directionsArray);
-})
-}
-question1()
+     const directionsArray = directions.split('');
+    const answer = directionsArray.reduce((acc, currentValue)=>{
+        if (currentValue === '('){
+            return acc += 1
+        } else if (currentValue ===')'){
+            return acc -= 1
+        } 
 
-//  When does santa enter basement
+        }, 0)
+        // see ttime to excecute finosehes here.
+        console.timeEnd('santa-time')
+        console.log('floor:',answer)
+    })
+}
+
+question1();
+
+//  When does santa enter basement ?
+    
+function question2() {
+    fs.readFile('./santa.txt', (err, data)=>{
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        const answer = directionArray.some((currentValue))
+    })
+}
